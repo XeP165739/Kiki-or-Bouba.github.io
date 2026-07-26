870dbb8885cca7de075c42303f49f4ece7413dda
XeP165739 <patlovesjen11@gmail.com>
2026-07-26 21:11:35 +0800

    feat: add result page with navigation, styling, and dynamic content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Landing } from './landing/landing';
 import { About } from './about/about';
+import { Result } from './result/result';
 
 export const routes: Routes = [
   { path : '', component : Landing },
-  { path : 'about', component : About }
+  { path : 'about', component : About },
+  { path : 'result', component : Result }
 ];
--- src/app/result/result.ts
+++ src/app/result/result.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { KikiShapeComponent } from "../components/shapes/kiki-shape/kiki-shape";
+import { BoubaShapeComponent } from "../components/shapes/bouba-shape/bouba-shape";
+import { KikiShapeOutlineComponent } from "../components/shapes/kiki-shape-outline/kiki-shape-outline";
+import { BoubaShapeOutlineComponent } from "../components/shapes/bouba-shape-outline/bouba-shape-outline";
+import { RouterLink, ɵEmptyOutletComponent } from "@angular/router";
+import { NgComponentOutlet } from "@angular/common";
 
 @Component({
   selector: 'app-result',
-  imports: [],
+  imports: [
+    KikiShapeComponent,
+    BoubaShapeComponent,
+    KikiShapeOutlineComponent,
+    BoubaShapeOutlineComponent,
+    RouterLink,
+    ɵEmptyOutletComponent,
+    NgComponentOutlet
+],
   templateUrl: './result.html',
   styleUrl: './result.css',
 })
-export class Result {}
+
+export class Result {
+  kiki_score = 50;
+  bouba_score = 100 - this.kiki_score;
+  mid_score = this.kiki_score;
+  result_title = 'Kiki';
+  result_headline = 'Sharp minded';
+
+  onStart() {}
+
+  onResult() {
+    return KikiShapeComponent;
+  }
+}
