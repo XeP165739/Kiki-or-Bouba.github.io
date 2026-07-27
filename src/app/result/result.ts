8084e30592abb69afd469a6c613a4be9745653a4
XeP165739 <patlovesjen11@gmail.com>
2026-07-27 23:44:33 +0800

    feat: remove navigation bar from landing page and update app structure

--- src/app/result/result.ts
+++ src/app/result/result.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { KikiShapeComponent } from "../components/shapes/kiki-shape/kiki-shape";
 import { BoubaShapeComponent } from "../components/shapes/bouba-shape/bouba-shape";
 import { KikiShapeOutlineComponent } from "../components/shapes/kiki-shape-outline/kiki-shape-outline";
 import { BoubaShapeOutlineComponent } from "../components/shapes/bouba-shape-outline/bouba-shape-outline";
 import { RouterLink, ɵEmptyOutletComponent } from "@angular/router";
 import { NgComponentOutlet } from "@angular/common";
-
 @Component({
   selector: 'app-result',
   imports: [
