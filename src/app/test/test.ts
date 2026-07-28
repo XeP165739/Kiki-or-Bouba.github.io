9fb16cd9255327ca707e3af56f2258a20aea6ff8
XeP165739 <patlovesjen11@gmail.com>
2026-07-28 19:42:56 +0800

    feat: add Test component with HTML, CSS, and unit tests

---
+++ src/app/test/test.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../node_modules/@angular/router/types/_router_module-chunk";
+
+@Component({
+  selector: 'app-test',
+  imports: [RouterOutlet],
+  templateUrl: './test.html',
+  styleUrl: './test.css',
+})
+export class Test {}
