8e948a257c1eb3373fe76fb937e5d0bb53c936ff
XeP165739 <patlovesjen11@gmail.com>
2026-07-28 19:52:23 +0800

    feat: update routing to include Test component and its child routes for quiz and result

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Landing } from './pages/landing/landing';
 import { About } from './pages/about/about';
-import { Result } from './result/result';
+import { Test } from './test/test';
 import { Main } from './main/main';
 
 export const routes: Routes = [
@@ -10,9 +10,16 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'landing', pathMatch: 'full' },
       { path: 'landing', component: Landing },
-      { path: 'about', component: About}
+      { path: 'about', component: About },
     ]
-  }
-  // { path : 'about', component : About },
-  // { path : 'result', component : Result }
+  },
+  {
+    path : 'test',
+    component : Test,
+    children: [
+      { path: '', redirectTo: 'quiz', pathMatch: 'full' },
+      { path: 'quiz', loadComponent: () => import('./pages/quiz/quiz').then((m) => m.Quiz) },
+      { path: 'result', loadComponent: () => import('./pages/result/result').then((m => m.Result)) }
+    ]
+  },
 ];
